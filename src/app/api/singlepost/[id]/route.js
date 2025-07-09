64b034f9f283721d908cc4ba12db0eb831b4b40c
kumarnivi <nivethikashivakumar56@gmail.com>
2025-07-09 11:40:10 +0530

    add the files

--- src/app/api/singlepost/[id]/route.js
+++ src/app/api/singlepost/[id]/route.js
@@ -4,14 +4,19 @@ import postModel from "../../../../../models/postModel";
 export async function GET(req, { params }) {
   try {
     await connectMOongo();
-    
-    // Optionally log the ID
-    // console.log("Post ID:", params.id);
+
+    if (!params?.id) {
+      return Response.json({ message: "Post ID is required" }, { status: 400 });
+    }
 
     const postData = await postModel.findOne({ _id: params.id });
 
+    if (!postData) {
+      return Response.json({ message: "Post not found" }, { status: 404 });
+    }
+
     return Response.json(postData);
   } catch (error) {
-    return Response.json({ message: error.message });
+    return Response.json({ message: error.message }, { status: 500 });
   }
 }
